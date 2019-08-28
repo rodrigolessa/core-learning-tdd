09a8a7410675b4d0eff1adea53e84c1843e734a7
Rodrigo Lessa <rodrigolsr@gmail.com>
2019-08-28 13:04:04 -0300

    validando representacao e adicao de algarismos

--- core.tddLearn.domain/Services/ConversorDeNumeroRomano.cs
+++ core.tddLearn.domain/Services/ConversorDeNumeroRomano.cs
@@ -1,12 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace core.tddLearn.domain
 {
     public class ConversorDeNumeroRomano
     {
+        private Dictionary<string, int> mapaDeAlgarismo = new Dictionary<string, int>(){
+            {"I", 1},
+            {"V", 5},
+            {"X", 10},
+            {"L", 50},
+            {"C", 100},
+            {"D", 500},
+            {"M", 1000}
+        };
+
         public int Converte(string numeroEmRomano)
         {
-            return 0;
+            int valor = 0;
+
+            for (int i = 0; i < numeroEmRomano.Length; i++)
+                valor += mapaDeAlgarismo[numeroEmRomano[i].ToString()];
+
+            return valor;
         }
     }
 }
--- core.tddLearn.test/ConversorDeNumeroRomanoTest.cs
+++ core.tddLearn.test/ConversorDeNumeroRomanoTest.cs
@@ -31,7 +31,9 @@ namespace Tests
         {
         }
 
-        [Test]
+        #region Primeiro Cenário - Representação dos Algarismos.
+
+        [Test, Category("Representação")]
         public void DeveEntenderOSimboloI()
         {
             ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
@@ -39,5 +41,250 @@ namespace Tests
 
             Assert.AreEqual(1, numero);
         }
+
+        [Test, Category("Representação")]
+        public void DeveEntenderOSimboloV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("V");
+
+            Assert.AreEqual(5, numero);
+        }
+
+        [Test, Category("Representação")]
+        public void DeveEntenderOSimboloX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("X");
+
+            Assert.AreEqual(10, numero);
+        }
+
+        [Test, Category("Representação")]
+        public void DeveEntenderOSimboloL()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("L");
+
+            Assert.AreEqual(50, numero);
+        }
+
+        [Test, Category("Representação")]
+        public void DeveEntenderOSimboloC()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("C");
+
+            Assert.AreEqual(100, numero);
+        }
+
+        [Test, Category("Representação")]
+        public void DeveEntenderOSimboloD()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("D");
+
+            Assert.AreEqual(500, numero);
+        }
+
+        [Test, Category("Representação")]
+        public void DeveEntenderOSimboloM()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("M");
+
+            Assert.AreEqual(1000, numero);
+        }
+
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        [TestCase("X", 10)]
+        [TestCase("L", 50)]
+        [TestCase("C", 100)]
+        [TestCase("D", 500)]
+        [TestCase("M", 1000)]
+        public void DeveEntenderOsSimbolos(string simbolo, int valor)
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte(simbolo);
+
+            Assert.AreEqual(valor, numero);
+        }
+
+        #endregion
+
+        #region Segundo Cenário - Dois símbolos em sequência.
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("II");
+
+            Assert.AreEqual(2, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoVI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("VI");
+
+            Assert.AreEqual(6, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoXI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("XI");
+
+            Assert.AreEqual(11, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoXX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("XX");
+
+            Assert.AreEqual(20, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoLI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("LI");
+
+            Assert.AreEqual(51, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoLV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("LV");
+
+            Assert.AreEqual(55, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoLX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("LX");
+
+            Assert.AreEqual(60, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoCI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("CI");
+
+            Assert.AreEqual(101, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoCX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("CX");
+
+            Assert.AreEqual(110, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoDX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("DX");
+
+            Assert.AreEqual(510, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoDC()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("DC");
+
+            Assert.AreEqual(600, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoMX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("MX");
+
+            Assert.AreEqual(1010, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoMD()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("MD");
+
+            Assert.AreEqual(1500, numero);
+        }
+
+        #endregion
+
+        #region Terceiro Cenário - Mais de dois símbolos em sequência.
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoXXII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("XXII");
+
+            Assert.AreEqual(22, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoMDX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("MDX");
+
+            Assert.AreEqual(1510, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoMDXV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("MDXV");
+
+            Assert.AreEqual(1515, numero);
+        }
+
+        [Test, Category("Adição")]
+        public void DeveEntenderDoisSimbolosComoDCLXII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("DCLXII");
+
+            Assert.AreEqual(662, numero);
+        }
+
+        #endregion
+
+        #region Quarto Cenário - .
+
+        [Test, Category("Subtração")]
+        public void DeveEntenderDoisSimbolosComoIV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("IV");
+
+            Assert.AreEqual(4, numero);
+        }
+
+        #endregion
     }
 }
